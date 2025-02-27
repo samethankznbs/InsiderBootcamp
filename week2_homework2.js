64de978155494f95b50f03118a128dc58859a2ab
samethankznbs <100464602+samethankznbs@users.noreply.github.com>
2025-02-27 15:36:21 +0300

    add-memory

--- week2_homework2.js
+++ week2_homework2.js
@@ -1,7 +1,7 @@
-function collatzSequenceLength(n) {
+function collatzSequenceLength(n, memo) {
     let count = 0;
-    
-    while (n !== 1) {
+    let original = n;
+    while (n !== 1 && !(n in memo)) {
         if (n % 2 === 0) {
             n = n / 2;
         } else {
@@ -9,17 +9,17 @@ function collatzSequenceLength(n) {
         }
         count++;
     }
-    
-    return count + 1; 
+    memo[original] = count + (memo[n] || 0);
+    return memo[original];
 }
 
 function findLongestCollatz(limit) {
     let longestStart = 0;
     let longestLength = 0;
-
+    let memo = {1: 1};
+    
     for (let i = 1; i < limit; i++) {
-        let length = collatzSequenceLength(i);
-        
+        let length = collatzSequenceLength(i, memo);
         if (length > longestLength) {
             longestLength = length;
             longestStart = i;
